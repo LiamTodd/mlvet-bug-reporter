be4af73ad6d906adde760b0d2cdedc96aeb33fff
LiamTodd <toddliam26@gmail.com>
2022-09-24 12:03:25 +1000

    add error handling

--- mlvet-bug-reporter/pages/api/reportBug.js
+++ mlvet-bug-reporter/pages/api/reportBug.js
@@ -5,15 +5,14 @@ export default async function handler(req, res) {
   const repoOwnerUsername = 'chloebrett';
   const repoName = 'mlvet';
   const bugReportLabelName = 'user-reported bug';
-  const bugReportLabelDesc = 'issues with this label originate from in-app user feedback'; 
+  const bugReportLabelDesc = 'issues with this label originate from in-app user feedback';
   const bugReportLabelColour = 'f59e42';
 
-
-  const octokit = new Octokit({
-    auth: authToken,
-  });
-
-
+  try{
+    const octokit = new Octokit({
+      auth: authToken,
+    });
+  
     // get labels
     const labelResponse = await octokit.request(
       'GET /repos/{owner}/{repo}/labels',
@@ -22,12 +21,12 @@ export default async function handler(req, res) {
         repo: repoName,
       }
     );
-
+  
     // check if bug report label exists
     const containsBugReportLabel = labelResponse.data
       .map((label) => label.name)
       .includes(bugReportLabelName);
-
+  
     // if not, create it
     if (!containsBugReportLabel) {
       await octokit.request('POST /repos/{owner}/{repo}/labels', {
@@ -38,16 +37,19 @@ export default async function handler(req, res) {
         color: bugReportLabelColour,
       });
     }
-
+  
     // create issue
-    const x = await octokit.request('POST /repos/{owner}/{repo}/issues', {
+    await octokit.request('POST /repos/{owner}/{repo}/issues', {
       owner: repoOwnerUsername,
       repo: repoName,
       title: req.body.title,
       body: req.body.description,
       labels: [bugReportLabelName],
     });
-
-    res.status(200).json({message: "success!"})
-  } 
+    res.status(200).json({ message: "success!" })
+  }catch(e){
+    res.status(500).json({message: e.message})
+  }
+  
+}
 
